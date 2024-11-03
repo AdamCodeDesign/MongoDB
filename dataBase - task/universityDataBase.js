7d8fe8f04d7348aa95f2d51d2b9fbb9286cdd0e2
LeshchuDev <leshchudev@gmail.com>
2024-11-03 14:08:58 +0100

    universityDataBase task complete

--- dataBase - task/universityDataBase.js
+++ dataBase - task/universityDataBase.js
@@ -32,13 +32,53 @@ async function addStudentToDB(client) {
   }
 }
 
+async function getStudents(collection, options = {}, resultLimit = 5) {
+  try {
+    const cursor = collection.find(options).limit(resultLimit);
+    let results = await cursor.toArray();
+
+    if (results.length > 0) {
+      console.log(`Found ${results.length} students: `);
+
+      results.forEach((element, i) => {
+        console.log(element);
+      });
+
+      return results;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+  return [];
+}
+
+async function updateStudent(collection, options, updateFields) {
+  await collection.updateOne(options, { $set: updateFields });
+}
+
+async function updateStudents(collection, options, updateFields) {
+  await collection.updateMany(options, { $set: updateFields });
+}
+
+async function deleteStudent(collection, options) {
+  return await collection.deleteMany(options);
+}
+
 async function main() {
   let client = null;
 
   try {
     client = await initDB();
-    await addStudentToDB(client);
     const collection = client.db("UniversityDataBase").collection("students");
+    const studentsData = await collection.find({}).toArray();
+
+    if (studentsData.length === 0) await addStudentToDB(client);
+
+    let result = await deleteStudent(collection, { firstName: "Aleksandra" });
+    console.log(`${result.deletedCount} students deleted`);
+
+    const students = await getStudents(collection, { age: { $lte: 30 } }, 100);
   } catch (error) {
     console.log(error);
   } finally {
